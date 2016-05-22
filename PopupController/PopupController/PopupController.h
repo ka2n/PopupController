1da0381028d905c9d5cad8dce9f475092533fae5
Katsuma Ito <katsumai@gmail.com>
2016-05-23 01:45:01 +0900

    Add framework target & project

---
+++ PopupController/PopupController/PopupController.h
@@ -0,0 +1,19 @@
+//
+//  PopupController.h
+//  PopupController
+//
+//  Created by ka2n on 2016/05/23.
+//  Copyright © 2016年 daisukeSato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopupController.
+FOUNDATION_EXPORT double PopupControllerVersionNumber;
+
+//! Project version string for PopupController.
+FOUNDATION_EXPORT const unsigned char PopupControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupController/PublicHeader.h>
+
+
